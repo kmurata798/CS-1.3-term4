64cb05aa2a9a0fe886b8920bd14a1b70070c70f8
kmurata798 <kmurata798@gmail.com>
2020-04-05 14:27:42 -0700

    [Add] Bases.py code

--- Code/number-bases/bases.py
+++ Code/number-bases/bases.py
@@ -17,12 +17,18 @@ def decode(digits, base):
     return: int -- integer representation of number (in base 10)"""
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-    # TODO: Decode digits from binary (base 2)
-    # ...
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
     # TODO: Decode digits from any base (2 up to 36)
-    # ...
+    decoded = 0
+    strings = string.digits + string.ascii_lowercase
+    print(strings)
+    digits = digits[::-1]
+    # digits = digits[::-1] which is reverse ordering of 
+
+    for i, number in enumerate(digits):
+        decoded += base**i * strings.index(number)
+        #enumerate adds a counter and an iterable, so the i and number will increase as loop continues
+
+    return decoded
 
 
 def encode(number, base):
@@ -34,12 +40,70 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
     # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
+    # given_num = 75
+    # given_num = 144
+    # first_num = 0
+    # second_num = 0
+
+    # remainder = given_num % 16
+    # given_num = int(given_num / 16)
+
+    # first_num = given_num
+    # print("first_num= " + str(first_num))
+    # second_num = remainder
+    # print("second_num= " + str(second_num))
+    # if second_num > 9:
+    #     if second_num == 10:
+    #         second_num = 'A'
+    #     elif second_num == 11:
+    #         second_num = 'B'
+    #     elif second_num == 12:
+    #         second_num = 'C'
+    #     elif second_num == 13:
+    #         second_num = 'D'
+    #     elif second_num == 14:
+    #         second_num = 'E'
+    #     elif second_num == 15:
+    #         second_num = 'F'
+    # if first_num > 9:
+    #     if first_num == 10:
+    #         first_num = 'A'
+    #     elif first_num == 11:
+    #         first_num = 'B'
+    #     elif first_num == 12:
+    #         first_num = 'C'
+    #     elif first_num == 13:
+    #         first_num = 'D'
+    #     elif first_num == 14:
+    #         first_num = 'E'
+    #     elif first_num == 15:
+    #         first_num = 'F'
+    # print("0x" + str(first_num) + str(second_num))
+
+    encoded = ''
+    strings = string.digits + string.ascii_lowercase
+    p = 0
+    if number == 0:
+        return 0
+    
+    while (base**p) <= number:
+        p += 1
+
+    while p > 0:
+        print('p:', p)
+        div = int(number/(base**(p-1)))
+        print('div:', div)
+        convert = min(div, (base-1))
+        print('convert', convert)
+        encoded += strings[convert]
+        print('encoded', encoded)
+        number = number - ( convert * base**(p-1))
+        print('#', number)
+        p -= 1
+        print('p',p)
+
+    return encoded
 
 
 def convert(digits, base1, base2):
@@ -51,14 +115,8 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
-    # TODO: Convert digits from base 2 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 2 to base 10 (and vice versa)
-    # ...
-    # TODO: Convert digits from base 10 to base 16 (and vice versa)
-    # ...
-    # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
+
+    return encode(decode(str(digits), base1), base2)
 
 
 def main():
